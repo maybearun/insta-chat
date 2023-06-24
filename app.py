d4efc51a0a3aa58f331d76a0eed4ba346b1708c3
arun <kanjoorarun@gmail.com>
2023-06-24 21:02:04 +0530

    Integrate openai with instagrapi

--- app.py
+++ app.py
@@ -5,31 +5,46 @@ import json
 
 from settings import settings
 
+from insta import login_user, reply 
 
-# async def get_completion(url:str, session: aiohttp.ClientSession, content:str = "hello"):
-#     data = {
-#         "model": settings.model,
-#         "messages": [{"role": 'user', "content": content }],
-#         "temperature" : 0.7,
-#     }
-#     headers = {
-#         "Content-type": "application/json",
-#         "Authorization": f"Bearer {settings.api_key}"
-#     }
-#     response = await session.post(
-#         url=url, headers=headers, data=json.dumps(data)
-#     )
-#     response_data = await response.json()
-#     print(response_data)
+URL = 'https://api.openai.com/v1/chat/completions'
 
-# async def main():
-#     url = settings.url
-#     async with aiohttp.ClientSession() as session:
-#         await get_completion(url=url, session=session)
+async def get_completion(url:str, session: aiohttp.ClientSession, content:str = "hello"):
+    data = {
+        "model": settings.model,
+        "messages": [{"role": 'user', "content": content }],
+        "temperature" : 0.7,
+    }
+    headers = {
+        "Content-type": "application/json",
+        "Authorization": f"Bearer {settings.api_key}"
+    }
+    response = await session.post(
+        url=url, headers=headers, data=json.dumps(data)
+    )
+    response_data = await response.json()
+    print(f"content {content}")
+    return(response_data['choices'][0]['message']['content'])
+
+async def main():
+    url = URL
+
+    cl = login_user()
     
+    while True:    
+        async with aiohttp.ClientSession() as session:
+            all_threads = cl.direct_threads(selected_filter="unread")
+            if all_threads:
+                for thread in all_threads:
+                    content = ""
+                    for msg in thread.messages:
+                        content= msg.text
+                    answer = await get_completion(url=url, session=session, content=content)
+                    reply(cl=cl, thread_id=thread.id, answer=answer)
+                        
 if __name__ == '__main__':
     t0 = time.time()
     print(settings.user)
-    # aio.run(main())
+    aio.run(main())
     t1 = round(time.time() - t0, 1)
     print(f"it took {t1} secs")
--- settings.py
+++ settings.py
@@ -2,10 +2,10 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     api_key: str
-    url: str
     model: str = 'gpt-3.5-turbo'
     user: str = 'user'
-
+    username :str
+    password :str
     class Config:
         env_file = ".env"
 settings = Settings()
